53658914a21b200623cfc69d4facbbfe1c4470e0
Payne Miller <paynemiller7792@gmail.com>
2021-12-07 23:29:56 -0500

    Create Upgrade base class and Health Potion sublass

---
+++ Source/TheHotGatesFinal/HealthPotion.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HealthPotion.generated.h"
+
+UCLASS()
+class THEHOTGATESFINAL_API AHealthPotion : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHealthPotion();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attributes")
+	float value;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/TheHotGatesFinal/Upgrade.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Upgrade.h"
+
+// Sets default values
+AUpgrade::AUpgrade()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUpgrade::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUpgrade::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
